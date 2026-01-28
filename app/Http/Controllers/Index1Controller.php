6912c7852bfae9a831ab9a6211535f2af3f686b4
furqo-n <muhammadnurfurqon3@students.unnes.ac.id>
2026-01-28 10:56:06 +0700

    fix 500 error

--- app/Http/Controllers/Index1Controller.php
+++ app/Http/Controllers/Index1Controller.php
@@ -12,7 +12,7 @@ class Index1Controller extends Controller
     {
         $projectChunks = DB::connection('mysql_admin')->table('portofolios')->latest()->get()->chunk(3);
         $hero = DB::connection('mysql_admin')->table('gambars')->where('kategori', 'Hero')->latest()->get();
-        $text1 = DB::connection('mysql_admin')->table('gambars')->where('id', '5')->first();
+        $text1 = DB::connection('mysql_admin')->table('gambars')->where('id', '5')->first() ?? (object) ['gambar' => 'default.jpg'];
         $kliens = DB::connection('mysql_admin')->table('kliens')->get()->groupBy('kategori');
 
         return view('index1', compact('projectChunks', 'hero', 'text1', 'kliens'));
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,17 +22,21 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         View::composer('*', function ($view) {
-            try {
-                $logodata = DB::connection('mysql_admin')->table('gambars')->where('id', '7')->first();
-                $logodata2 = DB::connection('mysql_admin')->table('gambars')->where('id', '8')->first();
-                $gambardata = DB::connection('mysql_admin')->table('gambars')->pluck('gambar', 'kategori');
+            $logodata = (object) ['gambar' => 'default_logo.png'];
+            $logodata2 = (object) ['gambar' => 'default_logo.png'];
+            $gambardata = collect();
 
-                $view->with('logo', $logodata);
-                $view->with('logo2', $logodata2);
-                $view->with('gambardata', $gambardata);
+            try {
+                $logodata = DB::connection('mysql_admin')->table('gambars')->where('id', '7')->first() ?? $logodata;
+                $logodata2 = DB::connection('mysql_admin')->table('gambars')->where('id', '8')->first() ?? $logodata2;
+                $gambardata = DB::connection('mysql_admin')->table('gambars')->pluck('gambar', 'kategori');
             } catch (\Exception $e) {
                 // Prevent boot errors if DB is missing configuration or connection
             }
+
+            $view->with('logo', $logodata);
+            $view->with('logo2', $logodata2);
+            $view->with('gambardata', $gambardata);
         });
     }
 }
